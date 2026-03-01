1d94bc9d5faa4534e00dc518c09be5e4cd3c91f8
KIEMTRANHANTHUC <levannam112233@gmail.com>
2026-03-01 11:30:57 +0700

    Update questions.js

--- questions.js
+++ questions.js
@@ -237,10 +237,20 @@ answers:[
 }
 ],
 
-bo2: [],
+bo2: [], 
 bo3: []
-
 };
+
+questionSets.bo2 = questionSets.bo1;
+questionSets.bo3 = questionSets.bo1;
+
+function shuffle(array){
+for(let i=array.length-1;i>0;i--){
+const j=Math.floor(Math.random()*(i+1));
+[array[i],array[j]]=[array[j],array[i]];
+}
+return array;
+}
 
 function shuffle(array){
 for(let i=array.length-1;i>0;i--){
